b83477f946a3e28a269451d0e0dbabb4ef31fe53
felipejcsouza <felipejcs@gmail.com>
2024-07-05 15:53:54 -0300

    inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+
+provider "aws" {
+  region  = "sa-east-1"
+  shared_credentials_files  = ["C:/Users/felip/.aws/credentials"]
+  profile = "terraform"
+}
+
+module "lambda"{
+    source = "./lambda"
+    count = 0
+}
