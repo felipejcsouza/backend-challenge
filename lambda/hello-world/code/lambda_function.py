a98b3283dd6400538b2c275545fb53a0ed57e4f9
felipejcsouza <felipejcs@gmail.com>
2024-07-08 14:41:20 -0300

    test workflow

--- lambda/hello-world/code/lambda_function.py
+++ lambda/hello-world/code/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 def lambda_handler(event, context):
-    result = "Teste execução função via lambda"
+    result = "Teste execução função via lambda - v0.1"
     return {
         'statusCode' : 200,
         'body': result
