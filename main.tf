fc801fbc2126ec40d39813ac0c95802c3482f2df
felipejcsouza <felipejcs@gmail.com>
2024-07-08 15:54:48 -0300

    teste de deploy 3

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    backend "s3" {
+      bucket = "challengetfs3"
+      key    = "terraform.tfstate"
+      region = "sa-east-1"
+    }
   }
 
   required_version = ">= 1.2.0"
