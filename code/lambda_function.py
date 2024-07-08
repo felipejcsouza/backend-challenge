39e4dc70741871c0908b33e56c644496eda91876
felipejcsouza <felipejcs@gmail.com>
2024-07-08 16:08:37 -0300

    teste de deploy 6

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 def lambda_handler(event, context):
-    result = "Teste execução função via lambda - v0.5"
+    result = "Teste execução função via lambda - v0.6"
     return {
         'statusCode' : 200,
         'body': result
